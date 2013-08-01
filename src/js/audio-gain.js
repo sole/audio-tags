5f97f9b49b5fb4a266ee9e7985e670d8381cb045
Soledad Penades <sole@mozilla.com>
2013-08-01 10:04:48 +0100

    almost there with the oscillator but the node seems missing in nightly???

--- src/js/audio-gain.js
+++ src/js/audio-gain.js
@@ -16,6 +16,16 @@
 
                 console.log('TODO: if this node has children, it has to initialise them too');
 
+                var audioNode = this.audioNode;
+
+                // TODO REFACTOR
+                Array.prototype.slice.apply(this.childNodes).forEach(function(node) {
+                    if(node.init) {
+                        node.init(audioContext);
+                        node.audioNode.connect(audioNode);
+                    }
+                });
+
             }
         }
     });
--- src/js/audio-oscillator.js
+++ src/js/audio-oscillator.js
@@ -7,6 +7,14 @@
                 // need to attach to the parent
                 console.log(this.parent);
             }
+        },
+        methods: {
+            init: function(audioContext) {
+                console.log('init oscillator', audioContext);
+                this.audioNode = audioContext.createOscillator();
+                // TODO apply config
+            }
         }
+        // TODO: setters/getters for wave type etc
     });
 })();
