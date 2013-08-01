b4b5cb307863d3618cff408c0a5e551352487508
Soledad Penades <sole@mozilla.com>
2013-08-01 09:54:42 +0100

    uhh

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ a-t
 ===
 
 mysterious new experiment
+
+## notes you might, or not, care about
+
+Nested nodes are created first, all the way up to the hierarchy root.
+Iterating: text nodes don't have a tagName (it's undefined).
--- src/js/audio-gain.js
+++ src/js/audio-gain.js
@@ -8,6 +8,15 @@
             inserted: function() {
                 console.log('inserted gain', this. parentNode);
             }
+        },
+        methods: {
+            init: function(audioContext) {
+                console.log('init!');
+                this.audioNode = audioContext.createGain();
+
+                console.log('TODO: if this node has children, it has to initialise them too');
+
+            }
         }
     });
 })();
