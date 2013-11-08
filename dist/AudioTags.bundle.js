9365d8e8c9d0ba7681c0097127f9f85608e7e147
sole <listas@soledadpenades.com>
2013-11-08 22:50:26 +0000

    filter: implement Q parameter

--- dist/AudioTags.bundle.js
+++ dist/AudioTags.bundle.js
@@ -3992,13 +3992,22 @@ function register() {
 				
 				var self = this;
 
-				this.innerHTML = 'FILTER<br /><label>frequency<input class="frequency" type="range" min="10" max="24000" /></label>'
+				this.innerHTML = 'FILTER<br />'
+					+ '<label>frequency<input class="frequency" type="range" min="10" max="24000" /></label><br />'
+					+ '<label>Q<input class="q" type="range" min="0.0001" max="1000" /></label><br />'
 					+ '<select class="type"></select>';
 				
 				this.frequencyInput = this.querySelector('.frequency');
 				this.frequencyInput.addEventListener('change', function(e) {
 					self.frequency = parseInt(this.value, 10);
 				}, false);
+
+
+				this.qInput = this.querySelector('.q');
+				this.qInput.addEventListener('change', function(e) {
+					self.q = parseFloat(this.value, 10);
+				}, false);
+
 
 				this.typeSelect = this.querySelector('.type');
 				this.typeSelect.addEventListener('change', function(e) {
@@ -4027,12 +4036,15 @@ function register() {
 				filter.connect(this.output);
 
 				this.frequencyInput.value = filter.frequency.value;
-				// TODO Same for Q, gain
+				this.qInput.value = filter.Q.value;
+
+				// TODO Same for gain
+				this.typeSelect.value = filter.type;
 			},
 		},
 
 		accessors: {
-			// TODO Q, gain
+			// TODO gain
 			frequency: {
 				set: function(v) {
 					this.filter.frequency.value = v;
@@ -4041,11 +4053,17 @@ function register() {
 			},
 			type: {
 				set: function(v) {
-					console.log('change type', v);
 					this.filter.type = v;
 				},
 				get: function() { return this.filter.type; }
+			},
+			q: {
+				set: function(v) {
+					this.filter.Q.value = v;
+				},
+				get: function() { return this.filter.Q; }
 			}
+
 		}
 
 	});
